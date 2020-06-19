3959922137f330553ad043c06af3964deeec7abf
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2020-06-19 11:18:26 +0200

    restored Table

--- Table.h
+++ Table.h
@@ -13,15 +13,6 @@ struct Table {
   Row header;
   Grid grid;
 
-  Table() = default;
-  Table(const Row &) {}
-
-  Table(const Table &) = delete;
-  Table &operator=(const Table &) = delete;
-  //   Table(Table &&) = default;
-  //   Table& operator=(Table &&) = default;
-  //   ~Table() = default;
-
   // later with C++20
   // bool operator==(const Table &) const = default;
 };
